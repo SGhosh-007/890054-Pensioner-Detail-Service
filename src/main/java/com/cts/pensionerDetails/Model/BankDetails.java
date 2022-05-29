8618d0305c8ed698423333c85ecf9e7195abb3cf
SGhosh-007 <gsusho123@gmail.com>
2022-05-30 00:32:43 +0530

    first commit

---
+++ src/main/java/com/cts/pensionerDetails/Model/BankDetails.java
@@ -0,0 +1,44 @@
+package com.cts.pensionerDetails.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ *  @author Sushobhan Ghosh
+ *  @author 890054
+ * This is BankDetail class which contains the Bank details
+ * like bankName, bankType, accountNumber
+ *
+ */
+
+@AllArgsConstructor
+@Getter
+public class BankDetails {
+	
+	
+	private String bankName;
+	private long accountNumber;
+	private String bankType;
+	public String getBankName() {
+		return bankName;
+	}
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
+	}
+	public long getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(long accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public String getBankType() {
+		return bankType;
+	}
+	public void setBankType(String bankType) {
+		this.bankType = bankType;
+	}
+	public BankDetails() {
+		
+	}
+
+}
